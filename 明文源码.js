44f0fddd3a156c639f1ee8f7b75888dffed1162b
Muspellheim-LG <3182533372@qq.com>
2025-03-22 08:31:32 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -1,8 +1,8 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let userID = '560aa743-16c8-4a42-9f57-8a1dd193fbfb';
-let proxyIP = 'icook.hk';
+let userID = '3b2ef903-a09d-406a-927b-347151205f7e';
+let proxyIP = 'www.visa.co.jp';
 //let sub = '';
 let subConverter = atob('U1VCQVBJLkNNTGl1c3Nzcy5uZXQ=');
 let subConfig = atob('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0FDTDRTU1IvQUNMNFNTUi9tYXN0ZXIvQ2xhc2gvY29uZmlnL0FDTDRTU1JfT25saW5lX01pbmlfTXVsdGlNb2RlLmluaQ==');
